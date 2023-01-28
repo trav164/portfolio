3c84dc1ac6eef5e665704304e7688a549f8b278c
trav164 <travis164@gmail.com>
2023-01-28 09:27:59 +0000

    updated favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 		<div class="mx-auto max-w-xl text-center">
 			<h1 class="text-8xl sm:text-9xl font-black font-bebas text-mid">Hi, I'm Lewis</h1>
 			<strong class="mt-4 pb-2 text-3xl sm:text-4xl italic text-brand">
-				I build things for the web.
+				I build things for the web...
 			</strong>
 			<p class="mt-4 sm:text-xl sm:leading-relaxed text-mid">
 				Th’art nesh thee nay lad soft lad wacken thi sen up t’foot o’ our stairs. Nay lad where’s
