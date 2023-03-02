90fb0f9c90a23ac19d0afc20182bb92d7c09564f
Lewis Travis <lewis.travis@ecalpha.onmicrosoft.com>
2023-03-02 16:02:31 +0000

    some fancy changes to portfolio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 	const skills: string[] = ['.NET', 'Node', 'Docker', 'Azure', 'Supabase', 'MongoDB', 'Postgres'];
 </script>
 
-<main class="mx-auto sm:w-[600px] w-96 p-6">
+<main class="mx-auto sm:w-[600px] w-96 p-6 flex flex-col gap-12">
 	<section>
 		<div class="pt-20">
 			<div class="flex justify-end">
@@ -37,20 +37,21 @@
 		</div>
 	</section>
 
-	<section class="pt-12">
-		<h3 class="text-dark dark:text-light font-semibold">About</h3>
-
+	<section class="">
+		<h3 class="text-dark dark:text-light font-semibold">Hi, I'm Lewis</h3>
+		<hr class="border-none w-12 h-1 rounded bg-gradient-to-r from-indigo-400 to-brand" />
 		<div class="pt-4 text-dark dark:text-mid">
-			Hi I'm Lewis. I've created this simple portfolio to learn new skills and expand my knowledge
-			as a software developer. I'm always keen to learn new technology and enjoy working in
-			customer-oriented environments delivering products from start to finish.
+			I've created this simple portfolio to learn new skills and expand my knowledge as a software
+			developer. I'm always keen to learn new technology and enjoy working in customer-oriented
+			environments delivering products from start to finish.
 		</div>
 	</section>
 
-	<section class="pt-12">
+	<section class="">
 		<h3 class="text-dark dark:text-light font-semibold">Work Experience</h3>
+		<hr class="border-none w-12 h-1 rounded bg-gradient-to-r from-indigo-400 to-brand" />
 
-		<div class="pt-6 flex gap-4 text-dark dark:text-light text-sm leading-6">
+		<div class="mt-6 flex gap-4 text-dark dark:text-light text-sm leading-6">
 			<div class="text-dark dark:text-light w-32">2019 - present</div>
 			<div class="flex flex-col w-full">
 				<span class="font-semibold text-dark dark:text-light">Software Engineer – CGI</span>
@@ -61,12 +62,12 @@
 					also upskilling in backend technologies in order become a more well-rounded developer.
 				</span>
 				<span class="text-brand dark:text-mid pt-2 italic"
-					>TS • Angular • NGRX • React • .NET • MongoDb • Material •
+					>TS • Angular • NGRX • React • .NET • MongoDB • Material •
 				</span>
 			</div>
 		</div>
 
-		<div class="pt-6 flex gap-4 text-light text-sm leading-6">
+		<div class="mt-6 flex gap-4 text-light text-sm leading-6">
 			<div class="text-dark dark:text-light w-32">2017 - 2018</div>
 			<div class="flex flex-col w-full">
 				<span class="font-semibold text-dark dark:text-light"
@@ -78,10 +79,11 @@
 		</div>
 	</section>
 
-	<section class="pt-12">
+	<section class="">
 		<h3 class="text-dark dark:text-light font-semibold">Side Projects</h3>
+		<hr class="border-none w-12 h-1 rounded bg-gradient-to-r from-indigo-400 to-brand" />
 
-		<div class="pt-6 flex gap-4 text-light text-sm leading-6">
+		<div class="mt-6 flex gap-4 text-light text-sm leading-6">
 			<div class="text-dark dark:text-light w-32">Ongoing</div>
 			<div class="flex flex-col w-full">
 				<span class="font-semibold text-dark dark:text-light">Recipe Amigo</span>
@@ -95,7 +97,7 @@
 			</div>
 		</div>
 
-		<div class="pt-6 flex gap-4 text-light text-sm leading-6">
+		<div class="mt-6 flex gap-4 text-light text-sm leading-6">
 			<div class="text-dark dark:text-light w-32">2022</div>
 			<div class="flex flex-col w-full">
 				<span class="font-semibold text-dark dark:text-light">Portfolio</span>
@@ -109,7 +111,7 @@
 			</div>
 		</div>
 
-		<div class="pt-6 flex gap-4 text-light text-sm leading-6">
+		<div class="mt-6 flex gap-4 text-light text-sm leading-6">
 			<div class="text-dark dark:text-light w-32">2022</div>
 			<div class="flex flex-col w-full">
 				<span>
@@ -145,38 +147,41 @@
 		</div>
 	</section>
 
-	<section class="pt-12">
+	<section class="">
 		<h3 class="text-dark dark:text-light font-semibold">Buzzwords (Skills)</h3>
-		<p class="text-brand dark:text-mid font-xs italic pb-2">🏆 Key skills highlighted</p>
+		<hr class="border-none w-12 h-1 rounded bg-gradient-to-r from-indigo-400 to-brand" />
+
+		<p class="text-brand dark:text-mid font-xs italic py-2">🏆 Key Skills</p>
 
 		<div class="grid grid-cols-2 md:grid-cols-3 pt-4 gap-4">
 			{#each keySkills as skill}
 				<button
-					class="py-2 px-2 flex items-center justify-center gap-2 shadow-lg no-underline rounded-full bg-brand text-light font-semibold text-sm cursor-auto"
+					class="py-2 px-2 flex items-center justify-center gap-2 shadow-lg no-underline rounded-full bg-gradient-to-tr from-indigo-600 to-brand text-light font-semibold text-sm cursor-auto"
 				>
 					🏆 {skill}</button
 				>
 			{/each}
 			{#each skills as skill}
 				<button
-					class="py-2 px-4 shadow-lg no-underline rounded-full bg-mid text-dark font-semibold text-sm cursor-auto"
+					class="py-2 px-4 shadow-lg no-underline rounded-full bg-gradient-to-tr from-mid to-gray-200 text-dark font-semibold text-sm cursor-auto"
 					>{skill}</button
 				>
 			{/each}
 		</div>
 	</section>
 
-	<section class="pt-12">
-		<h3 class="text-dark dark:text-light font-semibold">Links</h3>
+	<section class="">
+		<h3 class="text-dark dark:text-light font-semibold">My Socials</h3>
+		<hr class="border-none w-12 h-1 rounded bg-gradient-to-r from-indigo-400 to-brand" />
 
-		<div class="pt-6 flex gap-4 text-light text-sm leading-6">
+		<div class="mt-6 flex gap-4 text-light text-sm leading-6">
 			<div class="text-dark dark:text-mid w-32">Github</div>
 			<div class="flex flex-col w-full">
 				<span class="text-brand underline"><a href="https://github.com/trav164">@trav164</a></span>
 			</div>
 		</div>
 
-		<div class="pt-6 flex gap-4 text-light text-sm leading-6">
+		<div class="mt-6 flex gap-4 text-light text-sm leading-6">
 			<div class="text-dark dark:text-mid w-32">LinkedIn</div>
 			<div class="flex flex-col w-full">
 				<span class="text-brand underline"
