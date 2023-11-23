868328a379ccd1fee32ac9046e9498735cbfa0c3
trav164 <travis164@gmail.com>
2023-11-23 20:24:17 +0000

    Added some more

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,20 @@
 	import '../app.css';
 </script>
 
-<main
-	class="flex h-[100vh] items-center justify-center bg-avatar bg-opacity-20 md:px-32 lg:px-64 px-8 text-black py-12 md:mt-0"
->
-	<div class="max-w-6xl flex flex-col gap-24">
-		<slot />
+<main class="bg-gray-100 text-gray-700 lowercase">
+	<div class="flex flex-col min-h-screen">
+		<nav class="max-w-5xl mx-auto p-6 w-full flex items-center justify-between">
+			<span class="text-2xl font-semibold">Lewis.</span>
+
+			<ul class="flex flex-row gap-2 text-sm">
+				<li>About</li>
+				<li>Projects</li>
+			</ul>
+		</nav>
+
+		<!-- Content -->
+		<div class="mx-auto grow max-w-5xl px-6 flex flex-col gap-24">
+			<slot />
+		</div>
 	</div>
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from 'svelte';
 	import { annotate, annotationGroup } from 'rough-notation';
 	import avatar from '$lib/images/avatar.svg';
+	import Projects from '$lib/projects.svelte';
 
 	onMount(() => {
 		const name = annotate(document.querySelector('#name'), {
@@ -32,15 +33,49 @@
 	/>
 </svelte:head>
 
-<section class="flex flex-col-reverse md:flex-row justify-center gap-8 w-full">
+<section class="flex flex-col-reverse md:flex-row justify-center gap-8 mt-24">
+	<!-- INTRO -->
+	<div class="flex flex-col w-full gap-8">
+		<h1 class="font-bold text-5xl leading-relaxed">Hi! I'm <span>Lewis.</span></h1>
+
+		<p class="tracking-wider leading-relaxed text-xl -mt-4">
+			I've created this <span id="simple">simple</span> portfolio to learn new skills and expand my knowledge
+			as a software developer. I'm always keen to learn new technology and enjoy working in customer-oriented
+			environments delivering products from start to finish.
+		</p>
+
+		<div class="w-full items-center justify-center md:justify-start flex gap-4">
+			<a
+				class="bg-gray-900 text-gray-100 px-8 py-4 rounded font-semibold text-sm hover:bg-gray-800"
+				href="#"
+				aria-label="linkedin">LinkedIn</a
+			>
+			<a
+				class="outline outline-2 outline-gray-300 font-semibold text-sm rounded px-8 py-4 hover:bg-gray-200"
+				href="#"
+				aria-label="linkedin">Github</a
+			>
+		</div>
+	</div>
+
+	<!-- AVATAR -->
+	<div class="mx-auto">
+		<img class="md:h-96 md:w-96" alt="avatar" src={avatar} />
+	</div>
+</section>
+
+<!-- PROJECTS -->
+<section>
+	<Projects />
+</section>
+
+<!-- <section class="flex flex-col-reverse md:flex-row justify-center gap-8 w-full">
 	<div class="w-full flex flex-col gap-8">
 		<h1 class="font-semibold text-5xl leading-normal">
 			Hi! I'm <span id="name" class="text-gray-800">Lewis</span>.
 		</h1>
 		<p class="tracking-wider leading-relaxed text-xl">
-			I've created this <span id="simple">simple</span> portfolio to learn new skills and expand my knowledge
-			as a software developer. I'm always keen to learn new technology and enjoy working in customer-oriented
-			environments delivering products from start to finish.
+
 		</p>
 
 		<div class="w-full items-center justify-center md:justify-start flex gap-4">
@@ -63,4 +98,4 @@
 
 <div class="">
 	<h3 class="text-xl font-semibold">Projects</h3>
-</div>
+</div> -->
